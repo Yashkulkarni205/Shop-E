9dafd2fac452e8a61f3ca8f6ab288a95c32835e1
Yashkulkarni205 <yashkulkarni205@gmail.com>
2020-11-30 17:15:04 +0530

    updated settings

--- shope/shope/settings.py
+++ shope/shope/settings.py
@@ -56,8 +56,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 ROOT_URLCONF = 'shope.urls'
 
 TEMPLATES = [
@@ -147,5 +145,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS =True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER = 'yashkulkarni205@gmail.com'
+EMAIL_HOST_PASSWORD = 'twdvifqckgctebch'
