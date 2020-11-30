89d4bb7ee73b7ac32a1349fb23a4f7ae88ff4437
Yashkulkarni205 <yashkulkarni205@gmail.com>
2020-11-30 16:51:12 +0530

    updated settings

--- shope/shope/settings.py
+++ shope/shope/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'shope.urls'
 
