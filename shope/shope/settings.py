0801072461bdd417e9f151da5f13fec159cab11b
Yashkulkarni205 <yashkulkarni205@gmail.com>
2020-11-30 16:30:00 +0530

    updated settings

--- shope/shope/settings.py
+++ shope/shope/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['shopecommerce.herokuapp.com']
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +55,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'shope.urls'
 
@@ -128,8 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR,'shop/static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'shop/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'shop/static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'shop/staticfiles')
 
 # Where to go after logging in
 LOGIN_REDIRECT_URL = 'home'
