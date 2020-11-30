7f3e84fd1302892fb2fc3dba0f9431f369a2caa0
Yashkulkarni205 <yashkulkarni205@gmail.com>
2020-11-30 15:49:46 +0530

    updated settings

--- shope/shope/settings.py
+++ shope/shope/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fhvebbhn)3i7k@!so0842(puo-rw#gpta@5l)@#z7cesn3$^+9'
+# SECRET_KEY = 'fhvebbhn)3i7k@!so0842(puo-rw#gpta@5l)@#z7cesn3$^+9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shopecommerce.herokuapp.com']
 
 
 # Application definition
@@ -143,5 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS =True
-EMAIL_HOST_USER = 'yashkulkarni205@gmail.com'
-EMAIL_HOST_PASSWORD = 'LoneWalker@205'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
